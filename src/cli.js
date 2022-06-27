8ecd500d73a7a96cb913005abd7007efe2e30010
Анастасия <MrsDante@MacBook-Pro-Anastasia.local>
2022-06-27 17:57:49 +0300

    .

--- src/cli.js
+++ src/cli.js
@@ -4,8 +4,10 @@
 import readlineSync from 'readline-sync';
 // Wait for user's response.
 // eslint-disable-next-line import/prefer-default-export
-export const greetings = () => {
+const greetings = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 };
+
+export default greetings;
